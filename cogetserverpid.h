75ed8dd59fb49103bacdc2d08ce1b920ab183d7b
Kim Grasman <kim.grasman@gmail.com>
2012-12-27 15:18:08 +0100

    Updates Now pure C, no ATL dependency 1-byte structure packing of objrefhdr Update license header

--- cogetserverpid.h
+++ cogetserverpid.h
@@ -1,76 +1,84 @@
 /*******************************************************************************
- Copyright (c) 2012, Kim Gräsman
- All rights reserved.
+Copyright (c) 2012, Kim Gräsman
+All rights reserved.
 
- Released under the BSD license. For details, please see LICENSE file.
+Released under the Modified BSD license. For details, please see LICENSE file.
 
 *******************************************************************************/
-#ifndef __COGETSERVERPID_H__
-#define __COGETSERVERPID_H__
+#ifndef INCLUDED_COGETSERVERPID_H__
+#define INCLUDED_COGETSERVERPID_H__
 
-#include <atlbase.h>
+#include <objbase.h>
 
-HRESULT CoGetServerPID(IUnknown* punk, DWORD* pdwPID)
+/* This structure represents the OBJREF up to the PID at offset 52 bytes.
+   1-byte structure packing to make sure offsets are deterministic. */
+#pragma pack(push, 1)
+typedef struct tagCOGETSERVERPID_OBJREFHDR
+{
+  DWORD  signature;  /* Should be 'MEOW'. */
+  BYTE  padding[48];
+  USHORT  pid;
+} COGETSERVERPID_OBJREFHDR;
+#pragma pack(pop)
+
+inline HRESULT CoGetServerPID(IUnknown* punk, DWORD* pdwPID)
 {
   HRESULT hr;
+  IUnknown* pProxyManager = NULL;
+  IStream* pMarshalStream = NULL;
+  HGLOBAL hg = NULL;
+  COGETSERVERPID_OBJREFHDR *pObjRefHdr = NULL;
+  LARGE_INTEGER zero = {0};
 
   if(pdwPID == NULL) return E_POINTER;
   if(punk == NULL) return E_INVALIDARG;
 
-  // This structure represents the OBJREF
-  // up to the pid at offset 52 bytes
-  typedef struct tagOBJREFHDR
-  {
-    DWORD  signature;  // Should be 'MEOW'
-    BYTE  padding[48];
-    USHORT  pid;
-  } OBJREFHDR;
-
-  CComPtr<IUnknown> spProxyManager;
-  hr = punk->QueryInterface(IID_IProxyManager, (void**)&spProxyManager);
-  if(FAILED(hr))
-  {
-    // No standard proxy, we can't make any assumptions about remote PID
-    return hr;
-  }
-
-  // Marshal the interface to get a new OBJREF
-  CComPtr<IStream> spMarshalStream;
-  hr = ::CreateStreamOnHGlobal(NULL, TRUE, &spMarshalStream);
-  if(FAILED(hr)) return hr;
-
-  hr = ::CoMarshalInterface(spMarshalStream, IID_IUnknown, punk, MSHCTX_INPROC, NULL, MSHLFLAGS_NORMAL);
+  /* Make sure this is a standard proxy, otherwise we can't make any
+     assumptions about OBJREF wire format. */
+  hr = punk->QueryInterface(IID_IProxyManager, (void**)&pProxyManager);
   if(FAILED(hr)) return hr;
   
-  // We just created the stream, so go back to a raw pointer
-  HGLOBAL hg = NULL;
-  hr = ::GetHGlobalFromStream(spMarshalStream, &hg);
+  pProxyManager->Release();
+
+  /* Marshal the interface to get a new OBJREF. */
+  hr = ::CreateStreamOnHGlobal(NULL, TRUE, &pMarshalStream);
   if(SUCCEEDED(hr))
   {
-    // Start out pessimistic
-    hr = RPC_E_INVALID_OBJREF;
-
-    OBJREFHDR *pObjRefHdr = (OBJREFHDR*)GlobalLock(hg);
-    if(pObjRefHdr != NULL)
+    hr = ::CoMarshalInterface(pMarshalStream, IID_IUnknown, punk, 
+      MSHCTX_INPROC, NULL, MSHLFLAGS_NORMAL);
+    if(SUCCEEDED(hr))
     {
-      // Verify that the signature is MEOW
-      if(pObjRefHdr->signature == 0x574f454d)
+      /* We just created the stream so it's safe to go back to a raw pointer. */
+      hr = ::GetHGlobalFromStream(pMarshalStream, &hg);
+      if(SUCCEEDED(hr))
       {
-        // We got the remote PID
-        *pdwPID = pObjRefHdr->pid;
-        hr = S_OK;
+        /* Start out pessimistic. */
+        hr = RPC_E_INVALID_OBJREF;
+
+        pObjRefHdr = (COGETSERVERPID_OBJREFHDR*)GlobalLock(hg);
+        if(pObjRefHdr != NULL)
+        {
+          /* Verify that the signature is MEOW. */
+          if(pObjRefHdr->signature == 0x574f454d)
+          {
+            /* We got the remote PID! */
+            *pdwPID = pObjRefHdr->pid;
+            hr = S_OK;
+          }
+
+          GlobalUnlock(hg);
+        }
       }
 
-      GlobalUnlock(hg);
+      /* Rewind stream and release marshal data to keep refcount in order. */
+      pMarshalStream->Seek(zero, SEEK_SET, NULL);
+      CoReleaseMarshalData(pMarshalStream);
     }
+
+    pMarshalStream->Release();
   }
 
-  // Rewind stream and release marshal data to keep refcount in order
-  LARGE_INTEGER li = {0};
-  spMarshalStream->Seek(li, SEEK_SET, NULL);
-  CoReleaseMarshalData(spMarshalStream);
-  
   return hr;
 }
 
-#endif //__COGETSERVERPID_H__
+#endif // INCLUDED_COGETSERVERPID_H__
